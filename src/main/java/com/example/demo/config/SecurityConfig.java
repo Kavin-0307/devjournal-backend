d63b4687768297c45c075ee473807965101414be
Kavin-0307 <sharmakavin03@gmail.com>
2025-11-18 16:58:47 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -106,10 +106,12 @@ this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
 	    CorsConfiguration config = new CorsConfiguration();
 	    config.setAllowCredentials(true);
 	    config.setAllowedOriginPatterns(List.of(
-	        "http://localhost:5173",
-	        "http://127.0.0.1:5173",
-	        "https://*.up.railway.app"
-	    ));
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://*.up.railway.app",
+    "https://devjournal-frontend.vercel.app"
+));
+
 	    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 	    config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Origin", "Accept"));
 	    config.setExposedHeaders(List.of("Authorization"));
